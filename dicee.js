fcc0eea5272bdff0f7f92d3b359a8217929762f9
JayadevArun <jayadevarun03@gmail.com>
2023-05-17 22:18:44 +0530

    Updated Working

---
+++ dicee.js
@@ -0,0 +1,53 @@
+var randomNumber1=Math.random();
+var randomNumber2=(Math.floor(randomNumber1*6))+1;
+if(randomNumber2===1)
+{
+    document.querySelector("img.img1").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice1.png");
+}
+if(randomNumber2===2)
+{
+    document.querySelector("img.img1").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice2.png");
+}
+if(randomNumber2===3)
+{
+    document.querySelector("img.img1").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice3.png");
+}
+if(randomNumber2===4)
+{
+    document.querySelector("img.img1").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice4.png");
+}
+if(randomNumber2===5)
+{
+    document.querySelector("img.img1").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice5.png");
+}
+if(randomNumber2===6)
+{
+    document.querySelector("img.img1").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice6.png");
+}
+
+var randomNumber3=Math.random();
+var randomNumber4=Math.floor(randomNumber3*5)+1;
+if(randomNumber4==1)
+{
+    document.querySelector("img.img2").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice1.png");
+}
+if(randomNumber4===2)
+{
+    document.querySelector("img.img2").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice2.png");
+}
+if(randomNumber4===3)
+{
+    document.querySelector("img.img2").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice3.png");
+}
+if(randomNumber4===4)
+{
+    document.querySelector("img.img2").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice4.png");
+}
+if(randomNumber4===5)
+{
+    document.querySelector("img.img2").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice5.png");
+}
+if(randomNumber4===6)
+{
+    document.querySelector("img.img2").setAttribute("src","./Dicee Challenge - Starting Files/imagesnew/dice6.png");
+}
