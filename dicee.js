0e3464869d87fb1bbbc4f64147aec406ed682ccd
JayadevArun <jayadevarun03@gmail.com>
2023-05-18 13:45:46 +0530

    Updated Working

--- dicee.js
+++ dicee.js
@@ -2,54 +2,54 @@ var randomNumber1=Math.random();
 var randomNumber2=(Math.floor(randomNumber1*6))+1;
 if(randomNumber2===1)
 {
-    document.querySelector("img.img1").setAttribute("src","./imagesnew/dice1.png");
+    document.querySelector("img.img1").setAttribute("src","./dice1.png");
 }
 if(randomNumber2===2)
 {
-    document.querySelector("img.img1").setAttribute("src","./imagesnew/dice2.png");
+    document.querySelector("img.img1").setAttribute("src","./dice2.png");
 }
 if(randomNumber2===3)
 {
-    document.querySelector("img.img1").setAttribute("src","./imagesnew/dice3.png");
+    document.querySelector("img.img1").setAttribute("src","./dice3.png");
 }
 if(randomNumber2===4)
 {
-    document.querySelector("img.img1").setAttribute("src","./imagesnew/dice4.png");
+    document.querySelector("img.img1").setAttribute("src","./dice4.png");
 }
 if(randomNumber2===5)
 {
-    document.querySelector("img.img1").setAttribute("src","./imagesnew/dice5.png");
+    document.querySelector("img.img1").setAttribute("src","./dice5.png");
 }
 if(randomNumber2===6)
 {
-    document.querySelector("img.img1").setAttribute("src","./imagesnew/dice6.png");
+    document.querySelector("img.img1").setAttribute("src","./dice6.png");
 }
 
 var randomNumber3=Math.random();
 var randomNumber4=Math.floor(randomNumber3*5)+1;
 if(randomNumber4==1)
 {
-    document.querySelector("img.img2").setAttribute("src","./imagesnew/dice1.png");
+    document.querySelector("img.img2").setAttribute("src","./dice1.png");
 }
 if(randomNumber4===2)
 {
-    document.querySelector("img.img2").setAttribute("src","./imagesnew/dice2.png");
+    document.querySelector("img.img2").setAttribute("src","./dice2.png");
 }
 if(randomNumber4===3)
 {
-    document.querySelector("img.img2").setAttribute("src","./imagesnew/dice3.png");
+    document.querySelector("img.img2").setAttribute("src","./dice3.png");
 }
 if(randomNumber4===4)
 {
-    document.querySelector("img.img2").setAttribute("src","./imagesnew/dice4.png");
+    document.querySelector("img.img2").setAttribute("src","./dice4.png");
 }
 if(randomNumber4===5)
 {
-    document.querySelector("img.img2").setAttribute("src","./imagesnew/dice5.png");
+    document.querySelector("img.img2").setAttribute("src","./dice5.png");
 }
 if(randomNumber4===6)
 {
-    document.querySelector("img.img2").setAttribute("src","./imagesnew/dice6.png");
+    document.querySelector("img.img2").setAttribute("src","./dice6.png");
 }
 
 if(randomNumber2>randomNumber4)
